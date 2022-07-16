fc611f2d6851f3be1c88f6ef64d275afae7ea3e9
Piorosen <aoikazto@gmail.com>
2022-07-16 15:32:37 +0900

    Update namespace.go

--- database/namespace.go
+++ database/namespace.go
@@ -5,6 +5,14 @@ import (
 )
 
 type Namespace struct {
-	collection      *mongo.Collection
-	totalCollection *mongo.Collection
+	collection *mongo.Collection
+}
+
+func (self *Client) Namespace() *Namespace {
+	name := Namespace{}
+	if self.database == nil {
+		return nil
+	}
+	name.collection = self.database.Collection("namespace")
+	return &name
 }
